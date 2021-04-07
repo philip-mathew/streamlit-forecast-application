1db34534006095745040de9ef667747b20c0a17b
Philip Mathew <59157394+philip-mathew@users.noreply.github.com>
2021-04-07 02:24:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \”philipvino@gmail.com\”\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
