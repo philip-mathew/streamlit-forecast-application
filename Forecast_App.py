a92a9383956823b39a8703f6551d679db3fa4632
Philip Mathew <59157394+philip-mathew@users.noreply.github.com>
2021-04-11 22:24:17 -0400

    URL Update

--- Forecast_App.py
+++ Forecast_App.py
@@ -34,7 +34,7 @@ def main():
     
     # title & header
     st.title("Time Series Forecast Application")
-    st.markdown("Built by [Philip Mathew](https://github.com/philip-mathew)")
+    st.markdown("Built by [Philip Mathew](https://www.linkedin.com/in/philipmathew97/)")
     st.write("Data Science web application for automated forecasting of univariate time-series data.")
     st.write("Univariate time-series dataset (csv) can be uploaded below and forecast period can be inputed for automated forecasting using facebook prophet library.")
     st.markdown("Prophet forecasts time series data based on an additive model where non-linear trends are fit with yearly, weekly, and daily seasonality, plus holiday effects. It works best with time series that have strong seasonal effects and several seasons of historical data. Prophet is also robust to missing data and shifts in the trend, and typically handles outliers well.")
